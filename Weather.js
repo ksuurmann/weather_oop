ed1cc7bfa49d2e089f9dffccf4cb0e662d11de4e
ksuurmann <71810372+ksuurmann@users.noreply.github.com>
2020-12-15 12:10:55 +0200

    Weather data is readable

---
+++ Weather.js
@@ -0,0 +1,18 @@
+class Weather {
+  constructor(city) {
+    this.city = city;
+    this.key = "ae73c8b87baaba4d6569980d095c1206";
+  }
+
+  //data from API
+  async weatherData() {
+    const resp = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        this.city +
+        "&appid=" +
+        this.key
+    );
+    const data = await resp.json();
+    return data;
+  }
+}
